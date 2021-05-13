ac2ff70ca8822d353537252cce74e86301084eab
student <stu@ravtech.local>
2021-05-13 19:34:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const port = process.env.PORT || 8080;
+const express = require('express');
+const cors = require('cors');
+
+var app = express();
+
+app.use(cors);
+app.use(express.json());
+app.listen(port, console.log("Server up at port ", port));
