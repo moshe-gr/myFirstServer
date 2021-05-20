ce1e3dec635a8d486983ebccabfc92bd85f6e195
moshe-gr <mosegreenberg6@gmail.com>
2021-05-20 21:05:05 +0300

    ///

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const port = process.env.PORT || 8080;
+const dbPath = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/myFirst";
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const userRouts = require('./routs/userRouts.js');
 
 var app = express();
 
+mongoose.connect(dbPath);
+
 app.use(cors);
 app.use(express.json());
+app.use('/api/users', userRouts);
 app.listen(port, console.log("Server up at port ", port));
--- routs/userRouts.js
+++ routs/userRouts.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const userControler = require('../controlers/userControlers.js');
 
 var userRouts = express.Router();
-userRouts.post("/create");
-userRouts.put("/:_id");
-userRouts.delete("/:_id");
-userRouts.get("/getAll");
-userRouts.get("/:_id");
+userRouts.post("/create", userControler.createUser);
+userRouts.put("/:_id", userControler.updateUser);
+userRouts.delete("/:_id", userControler.deleteUser);
+userRouts.get("/getAll", userControler.getAll);
+userRouts.get("/:_id", userControler.getUser);
 
 module.exports = userRouts;
