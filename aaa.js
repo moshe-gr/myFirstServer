eb258dbab4993b52ae7fa5494a8f26f012112f8d
moshe-gr <mosegreenberg6@gmail.com>
2021-05-26 22:08:43 +0300

    face-detection

---
+++ aaa.js
@@ -0,0 +1,21 @@
+async function quickstart() {
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+
+    const fileName = './resources/WIN_20210526_20_47_22_Pro.jpg';
+
+    const [result] = await client.faceDetection(fileName);
+    const faces = result.faceAnnotations;
+    console.log('Faces:');
+    faces.forEach((face, i) => {
+    console.log(`  Face #${i + 1}:`);
+    console.log(`    Joy: ${face.joyLikelihood}`);
+    console.log(`    Anger: ${face.angerLikelihood}`);
+    console.log(`    Sorrow: ${face.sorrowLikelihood}`);
+    console.log(`    Surprise: ${face.surpriseLikelihood}`);
+    });
+}
+quickstart();
